a9610d78eef55cb7012dbc48a72d8f3a143d6d9e
NishesStha22 <sthanisi2020@gmail.com>
2024-08-07 11:46:24 +0545

    first commit

--- app/Http/Controllers/TasktableController.php
+++ app/Http/Controllers/TasktableController.php
@@ -3,10 +3,104 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tasktable;
 
 class TasktableController extends Controller
 {
-    public function index(){
-        
+    public function index()
+    {
+        try {
+            $tasks = Tasktable::where('delete_flags', false)->orderBy('id', 'asc')->get();
+            return response()->json([
+                'status' => 1,
+                'data' => $tasks
+            ]);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function store(Request $request)
+    {
+        // dd($request);
+        try {
+            // $validatedData = $request->validate([
+            //     'title' => 'required|string|max:255',
+            //     'status' => 'required|in:Done,Pending',
+            //     'date' => 'required|date',
+            // ]);
+
+            // dd()
+
+            $task = new Tasktable();
+            $task->title = $request->title;
+            $task->status = $request->status;
+            $task->delete_flags = false; 
+            $task->date = $request->date;
+            $task->save();
+
+            return response()->json([
+                'status' => 1,
+                'message' => 'Task added successfully.',
+                'data' => $task
+            ]);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            $task = Tasktable::where('id', $id)->where('delete_flags', false)->first();
+            return response()->json([
+                'status' => 1,
+                'data' => $task
+            ]);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $validatedData = $request->validate([
+                'title' => 'required|string|max:255',
+                'status' => 'required|in:Done,Pending',
+                'date' => 'required|date',
+            ]);
+
+            $task = Tasktable::where('id', $id)->where('delete_flags', false)->first();
+            $task->title = $validatedData['title'];
+            $task->status = $validatedData['status'];
+            $task->date = $validatedData['date'];
+            $task->save();
+
+            return response()->json([
+                'status' => 1,
+                'message' => 'Task updated successfully.',
+                'data' => $task
+            ]);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $task = Tasktable::where('id', $id)->where('delete_flags', false)->first();
+            $task->delete_flags = true;
+            $task->save();
+
+            return response()->json([
+                'status' => 1,
+                'message' => 'Task deleted successfully.',
+                'data' => $task
+            ]);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasktableController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
+
+
+
 });
+
+Route::group(['prefix' => 'tasks'],function () {
+    Route::get('/index', [TasktableController::class, 'index']); 
+    Route::post('/store', [TaskTableController::class, 'store']); 
+    Route::get('/show/{id}', [TaskTableController::class, 'show']); 
+    Route::put('/update/{id}', [TaskTableController::class, 'update']); 
+    Route::delete('/destory/{id}', [TasktableController::class, 'destroy']); 
+});
+
